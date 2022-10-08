34b9654e9fc8589d17cfd5a38f6a472232e15780
Ezra Huang <ezrahuang155@gmail.com>
2022-10-07 22:00:27 -0600

    fix formatting

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,49 @@
-import { REST, Routes } from 'discord.js'
-import dotenv from "dotenv"
-dotenv.config()
+import { REST, Routes } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!process.env.TOKEN || !process.env.CLIENT_ID) {
-  console.log('please get the env file from ezra huang: ezrahuang155@gmail.com')
+  console.log(
+    "please get the env file from ezra huang: ezrahuang155@gmail.com"
+  );
   process.exit(1);
 }
 
 const commands = [
   {
-    name: 'ping',
-    description: 'Replies with Pong!',
+    name: "ping",
+    description: "Replies with Pong!",
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands });
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+      body: commands,
+    });
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
 })();
 
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits } from "discord.js";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user!.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
   }
 });
 
