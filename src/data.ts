749cb58b1207ae647da7f483821e4f9239a90610
KevinKangGithub <haochenkang@gmail.com>
2022-10-08 17:39:38 -0600

    added data.ts

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+const meetingDates = [
+    "20221014", 
+    "20221021", 
+    "20221104", 
+    "20221118", 
+    "20221202", 
+    "20221209", 
+    "20221216", 
+    "20230210",
+    "20230224",
+    "20230303",
+    "20230317",
+    "20230414",
+    "20230421",
+    "20230428",
+    "20230512",
+    "20230519",
+    "20230525",
+    "20230602",
+]
+
+const months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+export { meetingDates, months };
