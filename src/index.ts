345a566bf9a6c2e27ef406b72716051c57c3f263
Ezra Huang <ezrahuang155@gmail.com>
2022-10-08 20:32:56 -0600

    fix merge conflict

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,15 @@ if (!process.env.TOKEN || !process.env.CLIENT_ID) {
 //  { name: "", description: ""},
 
 const commands = [
-  { name: "meeting", description: "Replies with the next meeting date."},
-  { name: "linktree", description: "Replies with the linktree link."},
-  { name: "lastlesson", description: "Replies with the previous lesson slides."},
-  { name: "help", description: "Replies with all possible commands."},
-  { name: "leaderboard", description: "Replies with codeforces leaderboard."},
-  { name: "codeforces", description: "Replies with codeforces information."},
+  { name: "meeting", description: "Replies with the next meeting date." },
+  { name: "linktree", description: "Replies with the linktree link." },
+  {
+    name: "lastlesson",
+    description: "Replies with the previous lesson slides.",
+  },
+  { name: "help", description: "Replies with all possible commands." },
+  { name: "leaderboard", description: "Replies with codeforces leaderboard." },
+  { name: "codeforces", description: "Replies with codeforces information." },
 ];
 // test
 
@@ -44,6 +47,8 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 })();
 
 import { Client, GatewayIntentBits } from "discord.js";
+import { getContestInfo, getGymContests } from "./codeforces/methods";
+import { meetingDates } from "./data";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on("ready", () => {
@@ -54,48 +59,48 @@ client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   //Return message with the date of the next CS club meeting
-  if (interaction.commandName === "meeting"){
-    for(let i = 0; i < meetingDates.length; i++){
-      if(meetingDates[i] >= currentDate()){
-        await interaction.reply("The next CS Club meeting is on " + dateToText(meetingDates[i]));
+  if (interaction.commandName === "meeting") {
+    for (let i = 0; i < meetingDates.length; i++) {
+      if (meetingDates[i] >= currentDate()) {
+        await interaction.reply(
+          "The next CS Club meeting is on " + dateToText(meetingDates[i])
+        );
         break;
       }
     }
-  //Return message with linktree
-  } else if (interaction.commandName === "linktree"){
-    await interaction.reply("Here is the Linktree: https://linktr.ee/westerncsclub")
-  //Return a list of commands
-  } else if (interaction.commandName === "help"){
-    let commandListString = ""
-    for(let i = 0; i < commands.length; i++){
-      commandListString += `**${commands[i].name}:** ${commands[i].description}\n`
+    //Return message with linktree
+  } else if (interaction.commandName === "linktree") {
+    await interaction.reply(
+      "Here is the Linktree: https://linktr.ee/westerncsclub"
+    );
+    //Return a list of commands
+  } else if (interaction.commandName === "help") {
+    let commandListString = "";
+    for (let i = 0; i < commands.length; i++) {
+      commandListString += `**${commands[i].name}:** ${commands[i].description}\n`;
     }
     let helpEmbed = {
-      title:"Help:",
-      fields:[
+      title: "Help:",
+      fields: [
         {
-          name:"Commands:",
-          value: commandListString
-        }
-      ]
-
-    }
-    await interaction.reply({embeds:[helpEmbed]})
-  } else if (interaction.commandName === "lastlesson"){
-    await interaction.reply("Filler Text")
-  } else if (interaction.commandName === "leaderboard"){
-    await interaction.reply("Filler Text")
+          name: "Commands:",
+          value: commandListString,
+        },
+      ],
+    };
+    await interaction.reply({ embeds: [helpEmbed] });
+  } else if (interaction.commandName === "lastlesson") {
+    await interaction.reply("Filler Text");
+  } else if (interaction.commandName === "leaderboard") {
+    await interaction.reply("Filler Text");
   }
 });
 
 client.login(process.env.TOKEN);
 
-import { makeCodeforcesApiCall } from "./codeforces/auth";
-import axios from "axios";
-import { meetingDates } from "./data";
-
-makeCodeforcesApiCall("/contest.list", {})
-.then((data) => {
-    console.log(data.data);
-  })
-  .catch((e) => console.log(e));
+(async () => {
+  // const contests = await getGymContests();
+  // console.log(contests);
+  const info = await getContestInfo(377892);
+  console.log(info);
+})();
